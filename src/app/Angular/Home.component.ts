79eb312871fa0cc45af27a7b2e0ea2114b54a1d1
rgeet <49391883+rgeet@users.noreply.github.com>
2020-02-05 12:25:53 +0000

    commiting to first time

---
+++ src/app/Angular/Home.component.ts
@@ -0,0 +1,10 @@
+import{Component,NgModule, OnInit} from '@angular/core';
+@Component({
+    selector:'app-home',
+    templateUrl:'./Home.component'
+})
+export class HomeComponent {
+    constructor (){}
+
+    
+}
